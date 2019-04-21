62a7e9cae73a05307a096bb10457c7cb0f594529
MrTiGeR007 <49088858+MrTiGeR007@users.noreply.github.com>
2019-04-21 19:01:22 +0530

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,263 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"─────────────────────────────────>
+echo $i" |"$cy"        INDIAN DARK WORLD ASSOCIATE"$i"    >
+echo $i" |"$me"───────────────────────────────────────────>
+echo $i" |"$pu" AUTHOR :"$ku" MrTiGeR007  [T1G3R]"$i"     >
+echo $i" |"$pu" FACEBOOK :"$ku" Tejash Kashyap"$i"        >
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.DK] [MR.V1RUS] [MR.X>
+echo $i" |"$cy"            ~ [cyber] [%MR.X%] [H4(K3R]"$i">
+echo $i" |"$pu" Contack WhatsApp :"$ku" 09540412418"$i"   >
+echo $i" |"$pu" Contack Gmail :"$ku" UnknownIamRaj@gmail.c>
+echo $i"["$bi"•"$i"]"$me"─────────────────────────────────>
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|>
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|>
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|>
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|>
+echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|>
+echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|>
+echo $i"────────────────────────────────"$i"    |"$cy" R"$>
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|>
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|>
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|>
+echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|>
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|>
+echo $i"|"$me"12"$i" |"$cy" BOT COMMENT FACEBOOK     "$i"|>
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|>
+echo $i"────────────────────────────────"$i"    |"$cy" O"$>
+echo $i"|"$me"14"$i" |"$cy" PHISHING FACEBOOK 1      "$i"|>
+echo $i"|"$me"15"$i" |"$cy" PHISHING FACEBOOK 2      "$i"|>
+echo $i"|"$me"16"$i" |"$cy" PHISHING FACEBOOK 3      "$i"|>
+echo $i"|"$me"17"$i" |"$cy" PHISHING FACEBOOK 4      "$i"|>
+echo $i"────────────────────────────────"$i"    |"$cy" D"$>
+echo $i"|"$me"18"$i" |"$cy" INSTALL THE MATERIAL COEG"$i"|>
+echo $i"|"$me"19"$i" |"$cy" EXIT PROGRAM             "$i"|>
+echo $i"────────────────────────────────    ─────"
+echo
+echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i"">
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Thank you for using my tool"
+sleep 2
+echo $i"When There Are Mistakes You Can Ask Me"
+sleep 2
+echo $ku"WhatsApp :"$i" 09540412418"
+echo $ku"Facebook :"$i" Tejash Kashyap"
+sleep 2
+echo $pur">> Thanks For Your Support <<"
+exit
+fi
